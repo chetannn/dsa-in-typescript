1a6b3f37d9f6419d58b74a215109b43f640de563
chetankharel <chetan@coveredpress.com>
2022-09-05 21:16:28 +0545

    wip

---
+++ src/algorithms/bubbleSort.ts
@@ -0,0 +1,14 @@
+export default function bubbleSort(arr: number[]) {
+  
+  let temp = 0
+
+  for(let i=0; i < arr.length; i++) {
+    for(let j = 1; j < arr.length - i; j++) {
+      if(arr[j] < arr[j-1]) {
+        temp = arr[j-1]
+        arr[j-1] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
+}
---
+++ test/algorithms/bubbleSort.test.ts
@@ -0,0 +1,15 @@
+import bubbleSort from "../../src/algorithms/bubbleSort"
+
+describe('bubbleSort tests', () => {
+
+    it('sorts positive numbers', () => {
+
+        const numbers = [5,7,2,28,7,1]
+
+        bubbleSort(numbers)
+
+        expect(numbers[0]).toBe(1)
+        expect(numbers[numbers.length - 1]).toBe(28)
+
+    })
+})
