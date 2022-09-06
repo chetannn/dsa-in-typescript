0bfbefcd9c54b41fa7936640bb040536f781e766
chetannn <chetankharel7@gmail.com>
2022-09-06 09:58:17 +0545

    some edge case tests for linear search

--- test/algorithms/linearSearch.test.ts
+++ test/algorithms/linearSearch.test.ts
@@ -3,6 +3,13 @@ import linearSearch from "../../src/algorithms/linearSearch"
 
 describe('linearSearch tests', function() {
 
+    it('should return false if array is empty', function() {
+
+        const found = linearSearch([], 10)
+
+        expect(found).toBeFalsy()
+    })
+
     it('should return true if element is found', function() {
 
     const nums = [4,6,4,2,6,9,12]
@@ -11,6 +18,14 @@ describe('linearSearch tests', function() {
 
     expect(found).toBeTruthy()
 
+    })
+
+    it('should return false if element is not found', function() {
+
+    const nums = [4,6,4,2,6,9,19,-1, -12]
+    const found = linearSearch(nums, 12)
+
+    expect(found).toBeFalsy()
 
     })
 
